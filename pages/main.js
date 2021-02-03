a561bcbce2b5a2dde4b4e4f5b898d066774dd9e2
Seth <seth@salukicorporation.com>
2021-02-03 18:06:09 +0000

    Massive improvements and table support
    Thanks GHOST

--- pages/main.js
+++ pages/main.js
@@ -1,13 +1,9 @@
-$(function () {
-    var $stats = $("#stats");
-    $.ajax({
-        type: "GET",
-        url: "https://covid19-api.org/api/status",
-        success: function(stats){
-        $.each(stats, function(i, stat)
-        {
-            $stats.append("<li>Country: "+ stat.country +", Cases: "+ stat.cases +" , Deaths: "+ stat.deaths + ", Recovered: "+ stat.recovered +", Last Updated:" + stat.last_update + "</li>");
-        })
-        }
-    });
-});
+var getData = {
+    "url": "https://covid19-api.org/api/status",
+    "method": "GET",
+    "timeout": 0,
+  };
+  
+  $.ajax(getData).done(function (response) {
+    
+  });
